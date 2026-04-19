9431b9af0402c168f4cbe2d0015c9e2b60e79863
Aryan Sharma <aryans2409@gmail.com>
2026-04-19 21:33:15 +0530

    disable auth for demo

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "http://16.171.43.79:5000/api",
 });
 
 API.interceptors.request.use((req) => {
@@ -12,4 +12,4 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
-export default API;
+export default API; 
